3a0349e6474f4a6e794f7f98b3702a4585fb88b8
behnambahrami1111 <behnambahrami31@gmail.com>
2024-02-18 00:22:16 +0330

    Respx added

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import httpx
-from httpx import Response
+from httpx import Response, codes, post, delete
 import os
 from dotenv import load_dotenv
 
@@ -9,6 +9,7 @@ load_dotenv()
 class ClusterAPIConsumer:
     def __init__(self):
         self.hosts = self._get_hosts()
+        self.endpoint_group = "/v1/group/"
 
     @staticmethod
     def _get_hosts():
@@ -21,17 +22,20 @@ class ClusterAPIConsumer:
         responses = []
         for host in self.hosts:
             try:
-                response = httpx.post(
-                    f"http://{host}/v1/group/", json={"groupId": group_id}
+                response = post(
+                    f"https://{host}{self.endpoint_group}", json={"groupId": group_id}
                 )
                 responses.append(response)
+
             except httpx.RequestError as e:
                 self.rollback_created_groups(group_id, responses)
                 raise e
-        return all(response.status_code == 201 for response in responses)
+        return all(response.status_code == codes.CREATED for response in responses)
 
     def rollback_created_groups(self, group_id: str, responses: list[Response]):
         for response in responses:
-            if response.status_code == 201:
+            if response.status_code == codes.CREATED:
                 host = response.request.url.host
-                httpx.delete(f"http://{host}/v1/group/")
+                delete(
+                    f"https://{host}{self.endpoint_group}", params={"groupId": group_id}
+                )
--- test_client.py
+++ test_client.py
@@ -1,5 +1,5 @@
-import httpx
-
+import respx
+from httpx import codes, Response, TimeoutException
 from client import ClusterAPIConsumer
 
 
@@ -8,48 +8,52 @@ class TestClusterAPIConsumer:
         self.client = ClusterAPIConsumer()
 
     @staticmethod
-    def mock_success_create_response():
-        response = httpx.Response(
-            status_code=201, json={"message": "Group created successfully"}
+    def create_group_success_response_mock():
+        response = Response(
+            status_code=codes.CREATED, json={"message": "Group created successfully"}
         )
         return response
 
     @staticmethod
-    def mock_bad_request_create_response():
-        response = httpx.Response(
-            status_code=400, json={"message": "Bad request. Perhaps the object exists."}
+    def create_bad_request_response_mock():
+        response = Response(
+            status_code=codes.BAD_REQUEST,
+            json={"message": "Bad request. Perhaps the object exists."},
         )
         return response
 
     @staticmethod
     def mock_success_delete_response():
-        response = httpx.Response(
-            status_code=200, json={"message": "Group deleted successfully"}
+        response = Response(
+            status_code=codes.OK, json={"message": "Group deleted successfully"}
         )
         return response
 
     @staticmethod
     def mock_success_get_response(group_id: str):
-        response = httpx.Response(status_code=200, json={"groupId": group_id})
+        response = Response(status_code=codes.OK, json={"groupId": group_id})
         return response
 
     @staticmethod
     def mock_timeout_response():
-        raise httpx.TimeoutException("Request timed out")
+        raise TimeoutException("Request timed out")
 
     @staticmethod
     def mock_internal_server_error_response():
-        response = httpx.Response(status_code=500)
+        response = Response(status_code=codes.INTERNAL_SERVER_ERROR)
         return response
 
-    def test_success_create_group(self, monkeypatch):
-        group_id = "example_group"
+    @respx.mock
+    def test_success_create_group_with_respx(self):
+        group_id = "example_group_id"
+        routes = [
+            respx.post(f"https://{host}{self.client.endpoint_group}").mock(
+                return_value=self.create_group_success_response_mock()
+            )
+            for host in self.client.hosts
+        ]
 
-        def mock_post():
-            return self.mock_success_create_response()
-
-        monkeypatch.setattr(httpx, "post", mock_post)
         result = self.client.create_group(group_id)
         assert result is True
-        for host in self.client.hosts:
-            assert httpx.post(f"http://{host}/v1/group/", json={"groupId": group_id})
+        for route in routes:
+            assert route.called
