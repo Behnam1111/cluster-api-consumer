60ced152689d0488b110d43a8054e95b115d818c
behnambahrami1111 <behnambahrami31@gmail.com>
2024-02-18 15:20:14 +0330

    Added unit test for failure scenario

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
-import httpx
-from httpx import Response, codes, post, delete
 import os
+from typing import List
+
+import httpx
 from dotenv import load_dotenv
+from httpx import Response, codes
 
 load_dotenv()
 
@@ -19,23 +21,45 @@ class ClusterAPIConsumer:
         return hosts_str.split(",")
 
     def create_group(self, group_id: str) -> bool:
-        responses = []
-        for host in self.hosts:
-            try:
-                response = post(
-                    f"https://{host}{self.endpoint_group}", json={"groupId": group_id}
-                )
-                responses.append(response)
+        responses: List[Response] = []
+        with httpx.Client() as client:
+            for host in self.hosts:
+                try:
+                    response = client.post(
+                        url=f"https://{host}{self.endpoint_group}",
+                        json={"groupId": group_id},
+                    )
 
-            except httpx.RequestError as e:
-                self.rollback_created_groups(group_id, responses)
-                raise e
-        return all(response.status_code == codes.CREATED for response in responses)
+                    if response.status_code != httpx.codes.CREATED:
+                        self.rollback_created_groups(group_id, responses)
+                        return False
+
+                    responses.append(response)
+
+                except httpx.RequestError:
+                    self.rollback_created_groups(group_id, responses)
+                    raise
+
+        return all(
+            response.status_code == httpx.codes.CREATED for response in responses
+        )
+
+    def get_group_by_id(self, host: str, group_id: str):
+        with httpx.Client() as client:
+            try:
+                response = client.get(
+                    url=f"https://{host}{self.endpoint_group}/{group_id}"
+                )
+                return response.json()
+            except httpx.RequestError:
+                raise
 
     def rollback_created_groups(self, group_id: str, responses: list[Response]):
-        for response in responses:
-            if response.status_code == codes.CREATED:
-                host = response.request.url.host
-                delete(
-                    f"https://{host}{self.endpoint_group}", params={"groupId": group_id}
-                )
+        with httpx.Client() as client:
+            for response in responses:
+                if response.status_code == codes.CREATED:
+                    host = response.request.url.host
+                    client.delete(
+                        url=f"https://{host}{self.endpoint_group}",
+                        params={"groupId": group_id},
+                    )
--- test_client.py
+++ test_client.py
@@ -1,5 +1,6 @@
 import respx
 from httpx import codes, Response, TimeoutException
+
 from client import ClusterAPIConsumer
 
 
@@ -7,19 +8,40 @@ class TestClusterAPIConsumer:
     def setup_method(self):
         self.client = ClusterAPIConsumer()
 
+    def _create_success_post_mock(self, endpoint: str, name: str):
+        return respx.post(endpoint, name=name).mock(
+            return_value=self._create_group_success_response()
+        )
+
+    def _create_failure_post_mock(self, endpoint: str, name: str):
+        return respx.post(endpoint, name=name).mock(
+            return_value=self._create_bad_request_response()
+        )
+
+    def _delete_success_mock(self, endpoint: str):
+        mock = respx.delete(endpoint).mock(
+            return_value=self._create_group_success_response()
+        )
+        return mock
+
     @staticmethod
-    def create_group_success_response_mock():
+    def _create_group_success_response():
         response = Response(
             status_code=codes.CREATED, json={"message": "Group created successfully"}
         )
         return response
 
     @staticmethod
-    def create_bad_request_response_mock():
+    def _create_bad_request_response():
         response = Response(
             status_code=codes.BAD_REQUEST,
             json={"message": "Bad request. Perhaps the object exists."},
         )
+        return response
+
+    @staticmethod
+    def _delete_group_success_response():
+        response = Response(status_code=codes.OK)
         return response
 
     @staticmethod
@@ -44,16 +66,43 @@ class TestClusterAPIConsumer:
         return response
 
     @respx.mock
-    def test_success_create_group_with_respx(self):
+    def test_success_create_group(self):
         group_id = "example_group_id"
         routes = [
-            respx.post(f"https://{host}{self.client.endpoint_group}").mock(
-                return_value=self.create_group_success_response_mock()
+            self._create_success_post_mock(
+                endpoint=f"https://{host}{self.client.endpoint_group}", name=host
             )
             for host in self.client.hosts
         ]
 
         result = self.client.create_group(group_id)
         assert result is True
+        for route in routes:
+            assert route.called
+
+    @respx.mock
+    def test_failure_create_group(self):
+        group_id = "example_group_id"
+        routes = [
+            self._create_success_post_mock(
+                endpoint=f"https://{self.client.hosts[0]}{self.client.endpoint_group}",
+                name=self.client.hosts[0],
+            ),
+            self._create_success_post_mock(
+                endpoint=f"https://{self.client.hosts[1]}{self.client.endpoint_group}",
+                name=self.client.hosts[1],
+            ),
+            self._create_failure_post_mock(
+                endpoint=f"https://{self.client.hosts[2]}{self.client.endpoint_group}",
+                name=self.client.hosts[2],
+            ),
+        ]
+        for host in self.client.hosts:
+            self._delete_success_mock(
+                endpoint=f"https://{host}{self.client.endpoint_group}?groupId={group_id}",
+            )
+
+        result = self.client.create_group(group_id)
+        assert result is False
         for route in routes:
             assert route.called
