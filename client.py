9115822d42578302999343fe72a837144cbb60ec
behnambahrami1111 <behnambahrami31@gmail.com>
2024-02-21 00:15:31 +0330

    Check if group_id already exists

--- client.py
+++ client.py
@@ -23,24 +23,27 @@ class ClusterAPIConsumer:
     async def create_group(self, group_id: str) -> bool:
         successful_created_hosts: List[str] = []
         for host in self.hosts:
-            response = await self.make_request("post", host, group_id)
-            if not self._create_group_id_was_successful(response):
-                await self.rollback_created_groups(group_id, successful_created_hosts)
+            response = await self._make_request("post", host, group_id)
+            if self._group_id_already_exists(response, host, group_id):
+                return False
+            
+            elif not self._create_group_id_was_successful(response):
+                await self._rollback_created_groups(group_id, successful_created_hosts)
                 return False
             successful_created_hosts.append(host)
         return True
-
+    
     async def delete_group(self, group_id: str) -> bool:
         successful_deleted_hosts: List[str] = []
         for host in self.hosts:
-            response = await self.make_request("delete", host, group_id)
+            response = await self._make_request("delete", host, group_id)
             if not self._delete_group_id_was_successful(response):
-                await self.rollback_deleted_groups(group_id, successful_deleted_hosts)
+                await self._rollback_deleted_groups(group_id, successful_deleted_hosts)
                 return False
             successful_deleted_hosts.append(host)
         return True
 
-    async def make_request(
+    async def _make_request(
         self, method: str, host: str, group_id: str
     ) -> httpx.Response | None:
         async with httpx.AsyncClient(trust_env=False) as async_client:
@@ -51,6 +54,8 @@ class ClusterAPIConsumer:
                     return await async_client.post(url, json={"groupId": group_id})
                 elif method == "delete":
                     return await async_client.delete(url, params={"groupId": group_id})
+                elif method == "get":
+                    return await async_client.get(url, params={"groupId": group_id})
             except httpx.RequestError as e:
                 logger.error(f"Request error: {e}")
             return None
@@ -64,27 +69,34 @@ class ClusterAPIConsumer:
         if response and response.status_code == httpx.codes.OK:
             return True
         return False
+    
+    async def _group_id_already_exists(self, response: httpx.Response | None, host: str, group_id: str) -> bool:
+        if response and response.status_code == httpx.codes.BAD_REQUEST:
+            response = await self._make_request("get", host, group_id)
+            if response and response.status_code == httpx.codes.OK:
+                return True
+        return False
 
-    async def rollback_created_groups(
+    async def _rollback_created_groups(
         self, group_id: str, hosts_to_rollback: List[str]
     ):
         for host in hosts_to_rollback:
-            result = await self.make_request("delete", host, group_id)
+            result = await self._make_request("delete", host, group_id)
             if not self._delete_group_id_was_successful(result):
                 q.enqueue_in(
                     time_delta=timedelta(seconds=60),
-                    func=self.rollback_created_groups,
+                    func=self._rollback_created_groups,
                     args=(group_id, [host]),
                 )
 
-    async def rollback_deleted_groups(
+    async def _rollback_deleted_groups(
         self, group_id: str, hosts_to_rollback: List[str]
     ):
         for host in hosts_to_rollback:
-            result = await self.make_request("post", host, group_id)
+            result = await self._make_request("post", host, group_id)
             if not self._create_group_id_was_successful(result):
                 q.enqueue(
-                    self.make_request,
+                    self._make_request,
                     retry=Retry(max=3, interval=10),
                     args=("post", host, group_id),
                 )
